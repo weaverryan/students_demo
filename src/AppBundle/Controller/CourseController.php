9f02605ecd1a71cb9a0a705be7df2258881b1395
Ryan Weaver <ryan@thatsquality.com>
2016-06-05 16:45:23 -0700

    basic courses page

---
+++ src/AppBundle/Controller/CourseController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CourseController extends Controller
+{
+    /**
+     * @Route("/courses", name="class_list")
+     */
+    public function listAction()
+    {
+        $courses = $this->getDoctrine()
+            ->getRepository('AppBundle:Course')
+            ->findAll();
+        
+        return $this->render('course/list.html.twig', [
+            'courses' => $courses,
+        ]);
+    }
+}
