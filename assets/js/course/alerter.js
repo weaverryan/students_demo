06e53b69260aad56e0f5198ffd4498ddcc01a1e3
Ryan Weaver <ryan@thatsquality.com>
2016-06-07 17:35:18 -0700

    Babel: Use es2015 class (for real)

--- assets/js/course/alerter.js
+++ assets/js/course/alerter.js
@@ -1,15 +1,16 @@
-var alerter = function($wrapper) {
-    this.initialize($wrapper);
-};
 
-$.extend(alerter.prototype, {
-    initialize: function($wrapper) {
+class alerter {
+    constructor($wrapper) {
+        this.initialize($wrapper);
+    }
+
+    initialize($wrapper) {
         this.$wrapper = $wrapper;
 
         this.$wrapper.on('click', '.js-alert', $.proxy(this._handleSendAlertClick, this));
-    },
+    }
 
-    _handleSendAlertClick: function(e) {
+    _handleSendAlertClick(e) {
         e.preventDefault();
 
         var message = $(e.currentTarget).data('message');
@@ -20,11 +21,11 @@ $.extend(alerter.prototype, {
         this.$wrapper.append(newLink);
 
         this.sendAlert(message);
-    },
+    }
 
-    sendAlert: function(msg) {
+    sendAlert(msg) {
         alert(msg);
     }
-});
+}
 
 export default alerter;
