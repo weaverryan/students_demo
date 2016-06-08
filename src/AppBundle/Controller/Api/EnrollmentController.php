e56ae9f5f5898a95dcbb828725b20d583ebf65ea
Ryan Weaver <ryan@thatsquality.com>
2016-06-07 17:35:22 -0700

    Enrollment: Add delete endpoint

---
+++ src/AppBundle/Controller/Api/EnrollmentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller\Api;
+
+use AppBundle\Entity\StudentEnrollment;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnrollmentController extends ApiBaseController
+{
+    /**
+     * @Route("/api/enrollments/{id}", name="api_enrollment_delete")
+     */
+    public function deleteAction(StudentEnrollment $enrollment)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($enrollment);
+        $em->flush();
+
+        return new Response(null, 204);
+    }
+}
