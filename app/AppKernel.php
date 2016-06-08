74fd0a911021ef9141344ea55704a8143b2de0cd
Ryan Weaver <ryan@thatsquality.com>
2016-06-07 17:35:12 -0700

    Exception: Install FOSRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new DoctrineMigrationsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
