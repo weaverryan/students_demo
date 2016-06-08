958e621f616c5955db0161089ad9aab4620b20ab
Ryan Weaver <ryan@thatsquality.com>
2016-06-07 17:35:09 -0700

    Test: Using ApiTestCase

--- tests/AppBundle/Controller/StudentControllerTest.php
+++ tests/AppBundle/Controller/StudentControllerTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\AppBundle\Controller;
 
+use AppBundle\Test\ApiTestCase;
 use GuzzleHttp\Client;
 
-class StudentControllerTest extends \PHPUnit_Framework_TestCase
+class StudentControllerTest extends ApiTestCase
 {
     public function testPOSTCreateStudent()
     {
@@ -14,13 +15,12 @@ class StudentControllerTest extends \PHPUnit_Framework_TestCase
             'lastName' => 'Weaver',
         ];
 
-        $client = new Client(['base_uri' => 'http://localhost:9007']);
-        $response = $client->post('/api/students', [
+        $response = $this->client->post('/api/students', [
             'body' => json_encode($data)
         ]);
 
         $this->assertEquals(201, $response->getStatusCode());
-        $responseBody = json_decode($response->getBody(), true);
-        $this->assertArrayHasKey('id', $responseBody);
+        $this->asserter()
+            ->assertResponsePropertyExists($response, 'id');
     }
 }
