880d7fb7524c8662e363675a50488493a44be888
Ryan Weaver <ryan@thatsquality.com>
2016-06-07 17:35:09 -0700

    Serializer: Install JMS

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new DoctrineMigrationsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
